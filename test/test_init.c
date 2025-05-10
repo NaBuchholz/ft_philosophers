c9367cdda698ba92f6ca67833d39cc0dea979d0f
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-05-10 16:48:01 -0300

    fix: update return values for main function and improve argument validation

--- test/test_init.c
+++ test/test_init.c
@@ -6,49 +6,43 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 16:26:50 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/05/10 16:33:16 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/05/10 16:47:48 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_data data;
+	t_data	data;
+	size_t	i;
 
 	if (ac < 5 || ac > 6)
 	{
 		printf("Error: invalid number of arguments\n");
-		return (1);
+		return (EXIT_FAILURE);
 	}
-
-	if (!init_program(&data, ++av))
+	if (init_program(&data, ++av) == EXIT_FAILURE)
 	{
 		printf("Failed to initialize program\n");
-		return (1);
+		return (EXIT_FAILURE);
 	}
-
 	/* Código para imprimir informações para verificação */
 	printf("Number of philosophers: %zu\n", data.philo_count);
 	printf("Time to die: %zu ms\n", data.time_to_die);
 	printf("Time to eat: %zu ms\n", data.time_to_eat);
 	printf("Time to sleep: %zu ms\n", data.time_to_sleep);
 	printf("Number of times each philosopher must eat: %zu\n", data.must_eat_count);
-
 	/* Imprimir informações dos filósofos para verificação */
-	size_t i;
-
 	i = 0;
 	while (i < data.philo_count)
 	{
-		printf("Philosopher %d: left fork = %p, right fork = %p\n",
+		printf("Philosopher %zu: left fork = %p, right fork = %p\n",
 			data.philos[i].id,
 			(void*)data.philos[i].l_fork,
 			(void*)data.philos[i].r_fork);
 		i++;
 	}
-
 	clean_all(&data);
-
-	return (0);
+	return (EXIT_SUCCESS);
 }
