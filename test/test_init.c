f0a6456b55f255bc1e4491f06f61acea0f7f094b
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-05-10 16:27:42 -0300

    test: add validation for argument count in main function

--- test/test_init.c
+++ test/test_init.c
@@ -5,12 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/10 14:30:00 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/05/10 15:14:46 by nbuchhol         ###   ########.fr       */
+/*   Created: 2025/05/10 16:26:50 by nbuchhol          #+#    #+#             */
+/*   Updated: 2025/05/10 16:27:14 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./includes/philo.h"
+#include "../includes/philo.h"
 
 int main(int ac, char **av)
 {
@@ -22,42 +22,20 @@ int main(int ac, char **av)
 		return (1);
 	}
 
-	// Inicializar data
-	if (!init_data(&data))
+	if (!init_program(&data, ++av))
 	{
-		printf("Failed to initialize data\n");
+		printf("Failed to initialize program\n");
 		return (1);
 	}
 
-	// Analisar argumentos
-	if (!parse_args(++av, &data))
-	{
-		printf("Failed to parse arguments\n");
-		return (1);
-	}
-
-	// Imprimir informações para verificação
+	/* Código para imprimir informações para verificação */
 	printf("Number of philosophers: %zu\n", data.philo_count);
 	printf("Time to die: %zu ms\n", data.time_to_die);
 	printf("Time to eat: %zu ms\n", data.time_to_eat);
 	printf("Time to sleep: %zu ms\n", data.time_to_sleep);
 	printf("Number of times each philosopher must eat: %zu\n", data.must_eat_count);
 
-	// Inicializar mutexes (se não for chamado dentro de init_data)
-	if (!init_mutexes(&data))
-	{
-		printf("Failed to initialize mutexes\n");
-		return (1);
-	}
-
-	// Inicializar filósofos (se não for chamado dentro de init_data)
-	if (!init_philos(&data))
-	{
-		printf("Failed to initialize philosophers\n");
-		return (1);
-	}
-
-	// Imprimir informações dos filósofos para verificação
+	/* Imprimir informações dos filósofos para verificação */
 	int i;
 
 	i = 0;
@@ -70,7 +48,7 @@ int main(int ac, char **av)
 		i++;
 	}
 
-	// clean_all(&data);
+	clean_all(&data);
 
 	return (0);
 }
